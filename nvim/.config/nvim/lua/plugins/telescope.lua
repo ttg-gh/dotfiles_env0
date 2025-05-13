5cf06fe9cf84247ac403acdf8d9f5d58c5eab2d2
ttg <eric15701570@gmail.com>
2025-05-13 22:03:43 +0900

    telescope show dotfiles

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,18 @@
 return {
-  "nvim-telescope/telescope.nvim", tag = '0.1.5',
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-  end
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		--vim.keymap.set("n", "<C-f>", builtin.find_files, {
+		--	hidden = true,
+		--})
+
+    vim.keymap.set('n', '<C-f>', function()
+      builtin.find_files({
+        hidden = true,
+        --no_ignore = true
+      })
+    end)
+	end
 }
