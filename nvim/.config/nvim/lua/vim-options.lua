f7dc3db70dea090dc6c1c7667cf3810c70ab06a4
ttg <eric15701570@gmail.com>
2025-05-14 03:30:56 +0900

    setup nvim lsp stuff

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -6,3 +6,15 @@ vim.wo.relativenumber = true
 
 -- 기본 설정
 vim.g.mapleader = " "
+
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●',  -- or '■', '▶', etc.
+    spacing = 4,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
