a07a182fa00f83c5702c77c470654b28d2e0600a
ttg <eric15701570@gmail.com>
2025-05-13 21:35:40 +0900

    setup nvim audocomplit

--- nvim/.config/nvim/lua/plugins.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  }
-}
