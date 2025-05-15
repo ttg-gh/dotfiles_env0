2bf87ab4ac685055cfd4bf733e54240394823877
ttg <eric15701570@gmail.com>
2025-05-16 02:03:48 +0900

    idk

--- hypr/.config/hypr/scripts/move_floating.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# workspace 전환 시 실행할 스크립트 예시
-CURRENT_WS=$(hyprctl workspaces -j | jq -r '.[] | select(.active==true).id')
-hyprctl clients -j \
-  | jq -r '.[] | select(.title | test("Picture[- ]in[- ]Picture"; "i")).address' \
-  | while read -r addr; do
-      hyprctl dispatch movetoworkspace "$CURRENT_WS" "$addr"
-    done
